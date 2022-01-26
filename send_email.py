c90594d9d29aa31e1d7806ee2e9f6fdf6f6c5af9
Huakun Shen <huakun.shen@huakunshen.com>
2022-01-25 22:48:14 -0500

    Fix a bug, wrong order of to and from address

--- send_email.py
+++ send_email.py
@@ -16,7 +16,7 @@ def send_email(subject, msg, to_email=EMAIL_ADDRESS):
         server.starttls()
         server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         message = 'Subject: {}\n\n{}'.format(subject, msg)
-        server.sendmail(EMAIL_ADDRESS, to_email, message)
+        server.sendmail(EMAIL_ADDRESS, to_email, message + " 1")
         server.quit()
         print("Success: Email sent!")
         return True
