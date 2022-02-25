6a18b1f03300762ddfdc0c8f6fb70aa91e50773f
Huakun Shen <huakun.shen@huakunshen.com>
2022-02-25 00:09:42 -0500

    Implement Application Key for

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, Response
 import logging
 from send_email import send_email
 
@@ -7,6 +7,8 @@ logging.basicConfig(filename='log.log', level=logging.CRITICAL, format="%(asctim
 app = Flask(__name__)
 
 RECEIVER_ADDRESS = os.environ.get('RECEIVER_ADDRESS')
+X_API_KEY = os.environ.get("X_API_KEY")
+
 
 @app.route('/', methods=['POST'])
 def log():
@@ -18,6 +20,9 @@ def log():
 
 @app.route('/email', methods=['POST'])
 def email():
+    if X_API_KEY is not None:
+        if 'X-Api-Key' not in request.headers or request.headers['X-Api-Key'] != X_API_KEY:
+            return Response("Not Authorized", status=401, mimetype='application/text')
     if 'data' in request.form:
         print(request.form['data'])
         if send_email("Subject: Simple Log Server", request.form['data'], RECEIVER_ADDRESS):
@@ -26,7 +31,7 @@ def email():
         else:
             app.logger.error('ERROR: cannot send email')
             return "no email"
-    return "no data in request"
+    return Response("no data in request", status=400, mimetype='application/text')
 
 
 if __name__ == "__main__":
